dfd901764eef5517584f9dcf28af0cd63089cdc1
David Lawrence Miller <github@ninepointeightone.net>
2013-12-12 14:56:07 +0000

    removed default "Detection function plot" from plot.ds

--- mrds/R/plot.ds.R
+++ mrds/R/plot.ds.R
@@ -327,8 +327,6 @@ plot.ds <- function(x, which=2, byvar="", breaks=NULL, nc=NULL,
 
     # use the user-supplied main= ...
     if(is.null(main)){
-       title("Detection function plot",cex.main=0.8)
-    }else{
        title(main,cex.main=0.8)
     }
   }
