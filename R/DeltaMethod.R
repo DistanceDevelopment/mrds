e5dc0620ac76b56e85a5b232e46b73c04ba9b02e
David Lawrence Miller <github@ninepointeightone.net>
2015-04-10 18:31:48 -0400

    documentation cleanup

--- R/DeltaMethod.R
+++ R/DeltaMethod.R
@@ -1,38 +1,19 @@
 #' Numeric Delta Method approximation for the variance-covariance matrix
 #'
-#' Computes delta method variance-covariance matrix of results of any generic
-#' function \code{fct} that computes a vector of estimates as a function of a
-#' set of estimated parameters \code{par}.
+#' Computes delta method variance-covariance matrix of results of any generic function \code{fct} that computes a vector of estimates as a function of a set of estimated parameters \code{par}.
 #'
-#' The delta method (aka propogation of
-#' errors is based on Taylor series approximation - see Seber's book on
-#' Estimation of Animal Abundance).  It uses the first derivative of \code{fct}
-#' with respect to \code{par} which is computed in this function numerically
-#' usign the central-difference formula.  It also uses the variance-covariance
-#' matrix of the estimated parameters which is derived in estimating the
-#' parameters and is an input argument.
+#' The delta method (aka propogation of errors is based on Taylor series approximation - see Seber's book on Estimation of Animal Abundance). It uses the first derivative of \code{fct} with respect to \code{par} which is computed in this function numerically using the central-difference formula. It also uses the variance-covariance matrix of the estimated parameters which is derived in estimating the parameters and is an input argument.
 #'
-#' The first argument of \code{fct} should be \code{par} which is a vector of
-#' parameter estimates. It should return a single value (or vector) of
-#' estimate(s).  The remaining arguments of \code{fct} if any can be passed to
-#' \code{fct} by including them at the end of the call to \code{DeltaMethod} as
-#' name=value pairs separated by commas as with all arguments.
+#' The first argument of \code{fct} should be \code{par} which is a vector of parameter estimates. It should return a single value (or vector) of estimate(s).  The remaining arguments of \code{fct} if any can be passed to \code{fct} by including them at the end of the call to \code{DeltaMethod} as \code{name=value} pairs.
 #'
-#' @param par vector of parameter values at which estimates should be
-#'   constructed
+#' @param par vector of parameter values at which estimates should be constructed
 #' @param fct function that constructs estimates from parameters \code{par}
 #' @param vcov variance-covariance matrix of the parameters
-#' @param delta proportional change in parameters used to numerically estimate
-#'   first derivative with central-difference formula
+#' @param delta proportional change in parameters used to numerically estimate first derivative with central-difference formula
 #' @param \dots any additional arguments needed by \code{fct}
 #' @export
-#' @return a list with values \item{variance}{estimated variance-covariance
-#'   matrix of estimates derived by \code{fct}} \item{partial}{ matrix (or
-#'   vector) of partial derivatives of \code{fct} with respect to the
-#'   parameters \code{par}}
-#' @note This is a generic function that can be used in any setting beyond the
-#'   mrds package.  However this is an internal function for mrds and the user
-#'   does not need to call it explicitly.
+#' @return a list with values \item{variance}{estimated variance-covariance matrix of estimates derived by \code{fct}} \item{partial}{ matrix (or vector) of partial derivatives of \code{fct} with respect to the parameters \code{par}}
+#' @note This is a generic function that can be used in any setting beyond the \code{mrds} package. However this is an internal function for \code{mrds} and the user does not need to call it explicitly.
 #' @author Jeff Laake
 #' @keywords utility
 DeltaMethod <- function(par, fct, vcov, delta, ...){
