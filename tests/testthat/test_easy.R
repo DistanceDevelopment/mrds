5f37afd623c42e142ac0040e2c1b85b8c5d016ac
David Lawrence Miller <github@ninepointeightone.net>
2015-05-05 15:08:27 -0400

    check error content when throwing input errors

--- tests/testthat/test_easy.R
+++ tests/testthat/test_easy.R
@@ -56,25 +56,29 @@ test_that("golf tee data gives the same results as Distance",{
   expect_error(result.cds<-ddf(dsmodel = ~cds(key = "hn",
                                               adj.series="cos",adj.order=1),
                                data = egdata, method = "ds",
-                               meta.data = list(width = 4)))
+                               meta.data = list(width = 4)),
+               "Cosine adjustments must be of order >2 for half-normal key functions")
 
   # half-normal with order 1 hermite adjustment
   expect_error(result.cds<-ddf(dsmodel = ~cds(key = "hn",
                                               adj.series="herm",adj.order=1),
                                data = egdata, method = "ds",
-                               meta.data = list(width = 4)))
+                               meta.data = list(width = 4)),
+               "Hermite polynomial adjustment terms of order < 4 selected")
 
   # hazard-rate with order 1 cosine adjustment
   expect_error(result.cds<-ddf(dsmodel = ~cds(key = "hr",
                                               adj.series="cos",adj.order=1),
                                data = egdata, method = "ds",
-                               meta.data = list(width = 4)))
+                               meta.data = list(width = 4)),
+               "Cosine adjustments must be of order >2 for hazard-rate key functions")
 
   # hazard-rate with order 1 simple poly adjustment
   expect_error(result.cds<-ddf(dsmodel = ~cds(key = "hr",
                                               adj.series="poly",adj.order=1),
                                data = egdata, method = "ds",
-                               meta.data = list(width = 4)))
+                               meta.data = list(width = 4)),
+               "Odd polynomial adjustment terms selected")
 
 })
 
