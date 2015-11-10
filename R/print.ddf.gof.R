d4285c6a02e39149b98cf89f92cf054d9ccdfb5b
David Lawrence Miller <github@ninepointeightone.net>
2015-11-10 11:59:07 -0500

    don't print chisquare if it's not there

--- R/print.ddf.gof.R
+++ R/print.ddf.gof.R
@@ -23,66 +23,69 @@ print.ddf.gof <- function(x,...){
 
   gof <- x
   cat("\nGoodness of fit results for ddf object\n")
-  cat("\nChi-square tests\n")
 
-  if(!is.null(gof$chisquare$chi2)){
-    cat("\nDistance sampling component:\n")
-  }
+  if(!is.null(gof$chisquare)){
+    cat("\nChi-square tests\n")
 
-  print(chitable(gof$chisquare$chi1$observed,gof$chisquare$chi1$expected))
+    if(!is.null(gof$chisquare$chi2)){
+      cat("\nDistance sampling component:\n")
+    }
 
-  if(!is.na(gof$chisquare$chi1$p)){
-    cat(paste("\nP = ",format(gof$chisquare$chi1$p,digits=5),
-              " with ",gof$chisquare$chi1$df," degrees of freedom\n",sep=""))
-  }else{
-    cat("\nNo degrees of freedom for test\n")
-  }
+    print(chitable(gof$chisquare$chi1$observed,gof$chisquare$chi1$expected))
 
-  if(!is.null(gof$chisquare$chi2)){
-    if(dim(gof$chisquare$chi2$observed)[1]==3){
-      Trial <- FALSE
+    if(!is.na(gof$chisquare$chi1$p)){
+      cat(paste("\nP = ",format(gof$chisquare$chi1$p,digits=5),
+                " with ",gof$chisquare$chi1$df," degrees of freedom\n",sep=""))
     }else{
-      Trial <- TRUE
+      cat("\nNo degrees of freedom for test\n")
     }
 
-    cat("\nMark-recapture component:\n")
+    if(!is.null(gof$chisquare$chi2)){
+      if(dim(gof$chisquare$chi2$observed)[1]==3){
+        Trial <- FALSE
+      }else{
+        Trial <- TRUE
+      }
 
-    if(Trial){
-      cat("Capture History 01\n")
-    }else{
-      cat("Capture History 10\n")
+      cat("\nMark-recapture component:\n")
+
+      if(Trial){
+        cat("Capture History 01\n")
+      }else{
+        cat("Capture History 10\n")
+      }
+
+      print(chitable(gof$chisquare$chi2$observed[1,],
+                     gof$chisquare$chi2$expected[1,]))
+
+      if(Trial){
+        cat("Capture History 11\n")
+      }else{
+        cat("Capture History 01\n")
+      }
+
+      print(chitable(gof$chisquare$chi2$observed[2,],
+                     gof$chisquare$chi2$expected[2,]))
+
+      if(!Trial){
+        cat("Capture History 11\n")
+        print(chitable(gof$chisquare$chi2$observed[3,],
+                       gof$chisquare$chi2$expected[3,]))
+      }
+
+      if(!is.na(gof$chisquare$chi2$p)){
+        cat(paste("\nMR total chi-square = ",
+                  format(gof$chisquare$chi2$chisq,digits=5),
+                  "  P = ",format(gof$chisquare$chi2$p,digits=5),
+                  " with ",gof$chisquare$chi2$df," degrees of freedom\n",sep=""))
+      }
+
+      cat(paste("\n\nTotal chi-square = ",
+                format(gof$chisquare$pooled.chi$chisq,digits=5),
+                "  P = ",format(gof$chisquare$pooled.chi$p,digits=5),
+                " with ",gof$chisquare$pooled.chi$df," degrees of freedom\n",
+                sep=""))
     }
-
-    print(chitable(gof$chisquare$chi2$observed[1,],
-                   gof$chisquare$chi2$expected[1,]))
-
-    if(Trial){
-      cat("Capture History 11\n")
-    }else{
-      cat("Capture History 01\n")
-    }
-
-    print(chitable(gof$chisquare$chi2$observed[2,],
-                   gof$chisquare$chi2$expected[2,]))
-
-    if(!Trial){
-      cat("Capture History 11\n")
-      print(chitable(gof$chisquare$chi2$observed[3,],
-                     gof$chisquare$chi2$expected[3,]))
-    }
-
-    if(!is.na(gof$chisquare$chi2$p)){
-      cat(paste("\nMR total chi-square = ",
-                format(gof$chisquare$chi2$chisq,digits=5),
-                "  P = ",format(gof$chisquare$chi2$p,digits=5),
-                " with ",gof$chisquare$chi2$df," degrees of freedom\n",sep=""))
-    }
-
-    cat(paste("\n\nTotal chi-square = ",
-              format(gof$chisquare$pooled.chi$chisq,digits=5),
-              "  P = ",format(gof$chisquare$pooled.chi$p,digits=5),
-              " with ",gof$chisquare$pooled.chi$df," degrees of freedom\n",
-              sep=""))
   }
 
   if(!is.null(gof$dsgof)){
