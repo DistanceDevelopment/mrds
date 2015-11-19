8e5a5ef8126cc12536e84de26060577d532d2d66
David Lawrence Miller <github@ninepointeightone.net>
2015-11-19 18:34:13 -0500

    pdf plotting for point transect models
    * plot.ds now has option pdf=TRUE to plot the pdf
    * covariate values are printed with dots (showpoints=TRUE)
    * only works for point transects (as lines look the same anyway)

--- R/plot.ds.R
+++ R/plot.ds.R
@@ -1,17 +1,10 @@
-#' Plot fit of detection functions and histograms of data from distance
-#' sampling model
+#' Plot fit of detection functions and histograms of data from distance sampling model
 #'
 #' Plots the fitted detection function(s) with a histogram of the observed distances to compare visually the fitted model and data.
 #'
-#' The structure of the histogram can be controlled by the user-defined
-#' arguments \code{nc} or \code{breaks}. The observation specific detection
-#' probabilities along with the line representing the fitted average detection
-#' probability.
+#' The structure of the histogram can be controlled by the user-defined arguments \code{nc} or \code{breaks}. The observation specific detection probabilities along with the line representing the fitted average detection probability.
 #'
-#' It is not intended for the user to call \code{plot.ds} but its arguments
-#' are documented here. Instead the generic \code{plot} command should be used
-#' and it will call the appropriate function based on the class of the
-#' \code{ddf} object.
+#' It is not intended for the user to call \code{plot.ds} but its arguments are documented here. Instead the generic \code{plot} command should be used and it will call the appropriate function based on the class of the \code{ddf} object.
 #'
 #' @aliases plot.ds
 #' @export
@@ -21,13 +14,8 @@
 #'               2 \tab histogram of observed distanes with fitted line and points (default)\cr}
 #' @param breaks user define breakpoints
 #' @param nc number of equal-width bins for histogram
-#' @param jitter.v scaling option for plotting points.  Jitter is applied to
-#'   points by multiplying the fitted value by a random draw from a normal
-#'   distribution with mean 1 and sd \code{jitter.v[j]}.  Where \code{j=1,2}
-#'   corresponds to observer \code{j} and \code{j=3} corresponds to
-#'   pooled/duplicate detections.
-#' @param showpoints logical variable; if \code{TRUE} plots predicted value
-#'   for each observation.
+#' @param jitter.v scaling option for plotting points.  Jitter is applied to points by multiplying the fitted value by a random draw from a normal distribution with mean 1 and sd \code{jitter.v[j]}.  Where \code{j=1,2} corresponds to observer \code{j} and \code{j=3} corresponds to pooled/duplicate detections.
+#' @param showpoints logical variable; if \code{TRUE} plots predicted value for each observation.
 #' @param subset subset of data to plot.
 #' @param pl.col colours plotting colours for obs 1, obs 2 detections.
 #' @param bw.col grayscale plotting colours for obs 1, obs 2 detections.
@@ -35,12 +23,9 @@
 #' @param pl.den shading density for plots of obs 1, obs 2 detections.
 #' @param pl.ang shading angle for plots of obs 1, obs 2 detections.
 #' @param main user-specfied plot title.
-#' @param pages the number of pages over which to spread the plots. For
-#'  example, if \code{pages=1} then all plots will be displayed on one page.
-#'  Default is 0, which prompts the user for the next plot to be displayed.
-#' @param \dots other graphical parameters, passed to the plotting functions
-#'   (\code{\link{plot}}, \code{\link{hist}}, \code{\link{lines}},
-#'   \code{\link{points}}, etc).
+#' @param pdf plot the histogram of distances with the PDF of the probability of detection overlaid. Ignored (with warning) for line transect models.
+#' @param pages the number of pages over which to spread the plots. For example, if \code{pages=1} then all plots will be displayed on one page. Default is 0, which prompts the user for the next plot to be displayed.
+#' @param \dots other graphical parameters, passed to the plotting functions (\code{\link{plot}}, \code{\link{hist}}, \code{\link{lines}}, \code{\link{points}}, etc).
 #' @return Just plots.
 #' @author Jeff Laake, Jon Bishop, David Borchers, David L Miller
 #' @keywords plot
@@ -49,6 +34,7 @@
 #' @importFrom stats rnorm
 #' @examples
 #' \donttest{
+#' # fit a model to the tee data
 #' data(book.tee.data)
 #' egdata <- book.tee.data$book.tee.dataframe
 #' xx <- ddf(dsmodel=~mcds(key="hn", formula=~sex),
@@ -68,18 +54,39 @@
 plot.ds <- function(x, which=2, breaks=NULL, nc=NULL,
                     jitter.v=rep(0,3), showpoints=TRUE, subset=NULL,
                     pl.col='black', bw.col=grey(0), black.white=FALSE,
-                    pl.den=rep(20,1), pl.ang=rep(-45,1), main=NULL,pages=0,...){
+                    pl.den=rep(20,1), pl.ang=rep(-45,1), main=NULL, pages=0,
+                    pdf=FALSE, ...){
 
-  #  Uses: detfct, histline, test.breaks
   model<-x
   lower <- 0
   vname <- "distance"
   xlab <- "Distance"
   dat <- model$data
 
+  # ignore pdf=TRUE with line transect data
+  if(pdf & !model$meta.data$point){
+    warning("Ignoring pdf=TRUE for line transect data")
+    pdf <- FALSE
+  }
+
   # decide which plots to show
   show <- rep(FALSE, 2)
   show[which] <- TRUE
+
+  ## Set printing options for plots:
+  # By default  pl.col=c('black'),
+  #             bw.col=c(grey(0))
+  # If greyscale plots are required use the following colours:
+  if(black.white){
+    byval1 <- bw.col[1]
+  }else{
+    # If colour plots are required use the following:
+    byval1 <- pl.col[1]
+  }
+  # Density of shaded lines - default is set all to 20
+  denval1 <- pl.den[1]
+  # Angle of shaded lines - default is set all to -45
+  angval1 <- pl.ang[1]
 
   # code from dpexplot:
   width <- model$meta.data$width
@@ -128,7 +135,7 @@ plot.ds <- function(x, which=2, breaks=NULL, nc=NULL,
   }
 
   if(length(model$fitted)==1){
-    pdot <- rep(model$fitted,sum(as.numeric(selected)))
+    pdot <- rep(model$fitted, sum(as.numeric(selected)))
   }else{
     pdot <- model$fitted[selected]
     Nhat <- sum(1/pdot)
@@ -170,7 +177,7 @@ plot.ds <- function(x, which=2, breaks=NULL, nc=NULL,
       breaks <- model$ds$aux$breaks
       nc <- length(breaks)-1
     }else{
-      breaks <- c(max(0,(max.range[1])),
+      breaks <- c(max(0, (max.range[1])),
                   max.range[1]+((max.range[2]-max.range[1])/nc)*(1:nc))
       if(breaks[1]>left){
         breaks <- c(left,breaks)
@@ -182,33 +189,17 @@ plot.ds <- function(x, which=2, breaks=NULL, nc=NULL,
   # test breaks for validity and reset as needed
   breaks <- test.breaks(breaks,model$meta.data$left,width)
   nc <- length(breaks)-1
-
-  #breaks<-seq(lower,upper,length=(nclass+1))
   lower <- min(breaks)
   upper <- max(breaks)
   dat <- dat[selected,]
-  keep <- dat[,vname]>=lower & dat[,vname]<=upper
-  h1 <- hist(dat[,vname][keep],breaks=breaks,plot=FALSE)
-  ymax <- max(h1$counts)
+  keep <- dat[ ,vname]>=lower & dat[ ,vname]<=upper
 
-  # Set printing options for plots:
-  # By default  pl.col=c('black'),
-  #             bw.col=c(grey(0))
+  # get the histogram object
+  hist.obj <- hist(dat[ ,vname][keep], breaks=breaks, plot=FALSE)
+  # what's the top of the largest bar?
+  ymax <- max(hist.obj$counts)
 
-  # If greyscale plots are required use the following colours:
-  if(black.white){
-    byval1 <- bw.col[1]
-  }else{
-    # If colour plots are required use the following:
-    byval1 <- pl.col[1]
-  }
-
-  # Density of shaded lines - default is set all to 20
-  denval1 <- pl.den[1]
-  # Angle of shaded lines - default is set all to -45
-  angval1 <- pl.ang[1]
-
-  # Scaling for grouped data:
+  # Rescaling for the histogram
   if(normalize & !point){
     bindata <- function(x, r, breaks){
       return(hist(r[r>=x[1] & r<=x[2]], breaks=breaks, plot=FALSE)$counts)
@@ -223,19 +214,23 @@ plot.ds <- function(x, which=2, breaks=NULL, nc=NULL,
     if(!point){
       expected.counts <- (breaks[2:(nc+1)]-breaks[1:nc])*(Nhat/breaks[nc+1])
     }else{
-      expected.counts <- -apply(matrix(c(breaks[2:(nc+1)]^2, breaks[1:nc]^2),
-                                       ncol=2, nrow=nc),
-                                1, diff)*(Nhat/breaks[nc+1]^2)
+      if(!pdf){
+        expected.counts <- -apply(matrix(c(breaks[2:(nc+1)]^2, breaks[1:nc]^2),
+                                         ncol=2, nrow=nc),
+                                  1, diff)*(Nhat/breaks[nc+1]^2)
+      }else{
+        expected.counts <- sum(hist.obj$counts)
+      }
     }
   }
 
-  hist.obj <- hist(dat[ ,vname][keep], breaks=breaks, plot=FALSE)
+  # rescale the histogram object by the expected counts
   hist.obj$density <- hist.obj$counts/expected.counts
   hist.obj$density[expected.counts==0] <- 0
   hist.obj$equidist <- FALSE
 
 
-  ### Actual plotting is here
+  ### Actual plotting starts here
 
   # do the paging, using devAskNewPage() means we can just call plots and
   # R will make the prompt for us
@@ -247,23 +242,44 @@ plot.ds <- function(x, which=2, breaks=NULL, nc=NULL,
     on.exit(par(opar))
   }
 
-  # Data summary plot for observer 1
+  # Data summary plot
   if(show[1]){
-    histline(h1$counts, breaks=breaks, lineonly=FALSE, ylim=c(0, ymax),
+    histline(hist.obj$counts, breaks=breaks, lineonly=FALSE, ylim=c(0, ymax),
              xlab=xlab, ylab="Frequency", angle=angval1,
              density=denval1, col=byval1, ...)
   }
 
-  # Detection function plot overlaid on histogram of observed
-  # distances - much of this code is taken from earlier plot.ds function.
+  ## Detection function plot overlaid on histogram of observed distances
   if(show[2]){
-    # Primary detection function
-    gxvalues <- detfct(xmat$distance, ddfobj, select=selected, width=width)
-    ymax <- max(hist.obj$density, max(gxvalues))
+    # Detection function/pdf values for points to be plotted
+    if(point & pdf){
+      point_vals <- distpdf(xmat$distance, ddfobj, width=width, point=TRUE,
+                                standardize=TRUE)/
+                    integratepdf(ddfobj, select=selected, width=width,
+                                 int.range=int.range, standardize=FALSE,
+                                 point=TRUE)
+      point_vals <- point_vals*sum(hist.obj$density*diff(breaks))
+    }else{
+      point_vals <- detfct(xmat$distance, ddfobj, select=selected, width=width)
+    }
+
+    # set y labels, limits and tick marks (det.plot) depending on if we
+    # are plotting PDF or df
+    if(pdf){
+      ylim <- c(0, max(hist.obj$density))
+      ylab <- "Probability density"
+      det.plot <- FALSE
+    }else{
+      ylim<-c(0, max(hist.obj$density, max(point_vals)))
+      ylab <- "Detection probability"
+      det.plot <- TRUE
+    }
+
+    # plot the histogram
     histline(hist.obj$density, breaks=breaks, lineonly=FALSE,
-             xlab=xlab, ylab="Detection probability", ylim=c(0, ymax),
+             xlab=xlab, ylab=ylab, ylim=ylim,
              angle=angval1, density=denval1, col=byval1,
-             det.plot=TRUE, ...)
+             det.plot=det.plot, ...)
 
     # when we have covariates
     if(hascov){
@@ -277,20 +293,41 @@ plot.ds <- function(x, which=2, breaks=NULL, nc=NULL,
         newdat$distance <- rep(x, nrow(newdat))
 
         detfct.values <- detfct(newdat$distance, ddfobj, select=selected,
-                               width=width)
+                                width=width)
 
         if(!normalize&range.varies){
           detfct.values[x<int.range[, 1] | x>int.range[, 2]] <- 0
         }
-        linevalues <- c(linevalues, sum(detfct.values/pdot)/sum(1/pdot))
+
+        if(point & pdf){
+          ## calculate the pdf of distances
+          # want r g(r) / int r g(r) dr
+
+          # this is 2 r g(r)/w^2
+          r_gr <- distpdf(newdat$distance, ddfobj, width=width, point=TRUE,
+                          standardize=TRUE)
+          # this is the value of int [2 r g(r) /w^2] dr
+          int_r_gr <- integratepdf(ddfobj, select=selected, width=width,
+                                   int.range=int.range, standardize=FALSE,
+                                   point=TRUE)
+          # so the pdf values are:
+          pdf_vals <- r_gr/int_r_gr
+
+          # now rescale such that area under pdf == area under histogram
+          rescale_f <- sum(hist.obj$density*diff(breaks))
+          vals <- pdf_vals * rescale_f
+          linevalues <- c(linevalues, sum(vals/pdot)/sum(1/pdot))
+        }else{
+          linevalues <- c(linevalues, sum(detfct.values/pdot)/sum(1/pdot))
+        }
       }
     # without covariates
     }else{
       if(!is.null(ddfobj$scale)){
-        ddfobj$scale$dm <- ddfobj$scale$dm[rep(1, length(xgrid)), ]
+        ddfobj$scale$dm <- ddfobj$scale$dm[rep(1, length(xgrid)), ,drop=FALSE]
       }
       if(!is.null(ddfobj$shape)){
-         ddfobj$shape$dm <- ddfobj$shape$dm[rep(1, length(xgrid)), ]
+        ddfobj$shape$dm <- ddfobj$shape$dm[rep(1, length(xgrid)), ,drop=FALSE]
       }
 
       # goofy workaround -- gamma is 0 at x=0, so add a little to the grid
@@ -298,15 +335,35 @@ plot.ds <- function(x, which=2, breaks=NULL, nc=NULL,
       if(ddfobj$type=="gamma"){
         xgrid[1] <- xgrid[1]+1e-6
       }
-      linevalues <- detfct(xgrid, ddfobj, width=width)
+
+      if(point & pdf){
+        ## calculate the pdf of distances
+        # want r g(r) / int r g(r) dr
+
+        # this is 2 r g(r)/w^2
+        r_gr <- distpdf(xgrid, ddfobj, width=width, point=TRUE,
+                        standardize=TRUE)
+        # this is the value of int [2 r g(r) /w^2] dr
+        int_r_gr <- integratepdf(ddfobj, select=TRUE, width=width,
+                                 int.range=int.range, standardize=FALSE,
+                                 point=TRUE)[1]
+        # so the pdf values are:
+        pdf_vals <- r_gr/int_r_gr
+
+        # now rescale such that area under pdf == area under histogram
+        rescale_f <- sum(hist.obj$density*diff(breaks))
+        linevalues <- pdf_vals * rescale_f
+      }else{
+        linevalues <- detfct(xgrid, ddfobj, width=width)
+      }
     }
 
     # actually plot the lines
     lines(xgrid, linevalues, col=byval1, ...)
 
     if(showpoints){
-      jitter.p <- rnorm(length(gxvalues), 1, jitter.v[1])
-      points(xmat$distance, gxvalues*jitter.p, col=byval1, ...)
+      jitter.p <- rnorm(length(point_vals), 1, jitter.v[1])
+      points(xmat$distance, point_vals*jitter.p, col=byval1, ...)
     }
 
     # use the user-supplied main= ...
