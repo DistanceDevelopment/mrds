7c1c5ceeca00e869349c5dd89e43708afaa75f80
David Lawrence Miller <github@ninepointeightone.net>
2015-11-25 14:58:24 -0500

    export assign.par for dsm

--- R/assign.par.R
+++ R/assign.par.R
@@ -1,22 +1,18 @@
 #' Extraction and assignment of parameters to vector
 #'
-#' Assigns parameters of a particular type (scale,
-#' shape, adjustments or g0 (p(0))) from the vector of parameters in
-#' \code{ddfobj}. All of the parameters are kept in a single vector for
-#' optimization even though they have very different uses.  \code{assign.par}
-#' parses them from the vector based on a known structure and assigns them into
-#' \code{ddfobj}.  \code{getpar} extracts the requested types to be extracted
-#' from \code{ddfobj}.
+#' Assigns parameters of a particular type (scale, shape, adjustments or g0 (p(0))) from the vector of parameters in \code{ddfobj}. All of the parameters are kept in a single vector for optimization even though they have very different uses. \code{assign.par} parses them from the vector based on a known structure and assigns them into \code{ddfobj}.
+#'
+#' \code{getpar} extracts the requested types to be extracted from \code{ddfobj}.
 #'
 #' @aliases assign.par
 #' @param ddfobj distance sampling object (see \code{\link{create.ddfobj}})
 #' @param fpar parameter vector
-#' @return index==FALSE, vector of parameters that were requested or
-#'   index==TRUE, vector of 3 indices for scale, shape, adjustment
+#' @return if \code{index==FALSE}, vector of parameters that were requested or \code{index==TRUE}, vector of 3 indices for scale, shape, adjustment
 #' @note Internal functions not intended to be called by user.
 #' @seealso getpar
 #' @author Jeff Laake
-#' @keywords utility
+#' @keywords utility internal
+#' @export
 assign.par <- function(ddfobj, fpar){
   index <- 1
   # Shape parameters
