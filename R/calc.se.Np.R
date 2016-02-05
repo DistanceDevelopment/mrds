4853982b60942d43cee2d61133c4ef187c2708b7
David Lawrence Miller <github@ninepointeightone.net>
2016-02-05 14:03:41 -0500

    better handling of NA Hessians in summary output

--- R/calc.se.Np.R
+++ R/calc.se.Np.R
@@ -8,28 +8,42 @@
 #' @author David L. Miller
 calc.se.Np <- function(model, avgp, n, average.p){
 
-  # calculate the variance-covariate matrix
-  vcov <- solvecov(model$hessian)$inv
+  # if we have any NAs in the hessian then everything is
+  # messed up, so we should just return NAs
+  if(any(is.na(model$hessian))){
+    se.obj <- list()
+    se.obj$Nhat.se <- NA
+    se.obj$average.p.se <- NA
+    model$hessian <- as.matrix(model$hessian)
+    se.obj$Nhatvar.list <-list(variance = matrix(NA, nrow(model$hessian),
+                                                 ncol(model$hessian)),
+                               partial  = matrix(NA, length(model$par),
+                                                 length(model$par)))
+    se.obj$vcov <- matrix(NA, nrow(model$hessian), ncol(model$hessian))
+  }else{
+    # calculate the variance-covariate matrix
+    vcov <- solvecov(model$hessian)$inv
 
-  # calculate Nhat uncertainty
-  Nhatvar.list <- DeltaMethod(model$par, NCovered, vcov, 0.001,
-                              model=model, group=TRUE)
-  Nhatvar <- Nhatvar.list$variance + sum((1-model$fitted)/model$fitted^2)
-  cvN <- sqrt(Nhatvar)/model$Nhat
+    # calculate Nhat uncertainty
+    Nhatvar.list <- DeltaMethod(model$par, NCovered, vcov, 0.001,
+                                model=model, group=TRUE)
+    Nhatvar <- Nhatvar.list$variance + sum((1-model$fitted)/model$fitted^2)
+    cvN <- sqrt(Nhatvar)/model$Nhat
 
-  # calculate the average p uncertainty
-  var.pbar.list <- prob.se(model, avgp, vcov)
-  covar <- t(Nhatvar.list$partial) %*% vcov %*% var.pbar.list$partial+
-               var.pbar.list$covar
-  var.pbar <- average.p^2*(cvN^2 + var.pbar.list$var/n^2-
-                                2*covar/(n*model$Nhat))
+    # calculate the average p uncertainty
+    var.pbar.list <- prob.se(model, avgp, vcov)
+    covar <- t(Nhatvar.list$partial) %*% vcov %*% var.pbar.list$partial+
+                 var.pbar.list$covar
+    var.pbar <- average.p^2*(cvN^2 + var.pbar.list$var/n^2-
+                                  2*covar/(n*model$Nhat))
 
-  # what should we return?
-  se.obj <- list()
-  se.obj$Nhat.se <- sqrt(Nhatvar)
-  se.obj$average.p.se <- sqrt(var.pbar)
-  se.obj$Nhatvar.list <- Nhatvar.list
-  se.obj$vcov <- vcov
+    # what should we return?
+    se.obj <- list()
+    se.obj$Nhat.se <- sqrt(Nhatvar)
+    se.obj$average.p.se <- sqrt(var.pbar)
+    se.obj$Nhatvar.list <- Nhatvar.list
+    se.obj$vcov <- vcov
+  }
 
   return(se.obj)
 }
