4f1077931e09d358f9a29644594af75abaf2dc57
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2016-08-09 23:40:26 +0100

    import setNames from stats

--- R/setinitial.ds.R
+++ R/setinitial.ds.R
@@ -18,7 +18,7 @@
 #'   \item{shape}{vector of initial shape parameter values}
 #'   \item{adjustment}{vector of initial adjustment function parameter values}
 #' @author Jeff Laake, Dave Miller
-#' @importFrom stats lm
+#' @importFrom stats lm setNames
 setinitial.ds <- function(ddfobj, width, initial, point){
 
   ftype <- ddfobj$type
