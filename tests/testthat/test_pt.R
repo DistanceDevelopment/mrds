2fc034488c421750cbb5ef665d142a3ab4859fa1
David Lawrence Miller <github@ninepointeightone.net>
2017-11-17 10:58:12 +0000

    add simulation point transect tests

--- tests/testthat/test_pt.R
+++ tests/testthat/test_pt.R
@@ -23,3 +23,48 @@ test_that("Point transect example from distance gives same results",{
 
 
 })
+
+
+context("Point transects: simulated data")
+
+test_that("Nothing has changed?", {
+
+  # thanks to Tiago Marques for this example
+  set.seed(123)
+  n <- 1000
+  w <- 8000
+  sigma <- 3000
+  xs <- runif(n,-w,w)
+  ys <- runif(n,-w,w)
+  rs <- sqrt(xs^2+ys^2)
+  gx <- function(x,sigma){exp(-(x/sigma)^2)}
+  px <- gx(rs,sigma)
+  pdet <- runif(n,0,1)
+  det <- px>pdet
+  data4D <- data.frame(distance=rs[det==1])
+  data4D$object <- 1:nrow(data4D)
+  data4D$detected <- rep(1, nrow(data4D))
+
+
+  # half-normal
+  xx <- ddf(dsmodel=~cds(key="hn", formula=~1), data=data4D,
+            method="ds",
+            meta.data=list(point=TRUE, width=8000))
+
+  expect_that(xx$par, equals(7.6438325, tol=par.tol))
+  expect_that(xx$lnl, equals(-1066.120945, tol=lnl.tol))
+  expect_that(summary(xx)$average.p, equals(0.1361185, tol=par.tol))
+
+
+  # hazard-rate
+  xx <- ddf(dsmodel=~cds(key="hr", formula=~1), data=data4D,
+            method="ds",
+            meta.data=list(point=TRUE, width=8000))
+
+  expect_that(unname(xx$par), equals(c(1.554381, 7.943638), tol=par.tol))
+  expect_that(xx$lnl, equals(-1069.143006, tol=lnl.tol))
+  expect_that(summary(xx)$average.p, equals(0.1862252, tol=par.tol))
+
+
+
+})
