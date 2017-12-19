c29cd8055d05d07a1addd75cecf8f1c63dbb09af
David Lawrence Miller <github@ninepointeightone.net>
2017-12-19 17:32:08 +0000

    Encounter rate variance for point transects when points were not all sampled a n equal number of times was incorrect. mrds now uses the P3 estimator from Fewst er et al (2009) for point transect encounter rate variance.

--- tests/testthat/test_pt.R
+++ tests/testthat/test_pt.R
@@ -68,3 +68,75 @@ test_that("Nothing has changed?", {
 
 
 })
+
+
+context("Point transects: variance")
+
+test_that("P3 variance estimator", {
+
+  # use the pt example data
+  data(ptdata.distance)
+
+  # generate some effort data
+  obs <- unique(ptdata.distance[, c("object", "Sample.Label", "Region.Label")])
+  sample <- unique(ptdata.distance[, c("Sample.Label", "Region.Label")])
+  region <- data.frame(Region.Label=1, Area=1)
+  ptdata.distance$size <- 1
+
+  # fit a model
+  xx <- ddf(dsmodel=~cds(key="hn", formula=~1), data=ptdata.distance,
+            method="ds",
+            meta.data=list(point=TRUE,width=max(ptdata.distance$distance)))
+
+  # with equal effort
+  sample$Effort <- 1
+  d <- dht(xx, obs.table=obs, region.table=region, sample.table=sample)
+  expect_equal(d$individuals$N$se, 0.001000225155)
+
+  # make effort unequal
+  set.seed(1234)
+  sample$Effort <- sample(1:3, nrow(sample), replace=TRUE)
+  d <- dht(xx, obs.table=obs, region.table=region, sample.table=sample)
+  expect_equal(d$individuals$N$se, 0.0007365085886)
+
+
+  # save code for later for testing...
+  ## pt encounter-rate variance "P3" from Fewster et al (2009)
+  #tk <- sample$Effort
+  #K <- nrow(sample)
+  #T <- sum(tk)
+
+  ## merge things
+  #mm <- merge(region, sample, by="Region.Label")
+  #mm <- merge(mm, obs, by="Sample.Label")
+  #mm <- merge(mm, ptdata.distance, by="object")
+  #mm$N <- mm$size/predict(xx)[[1]]
+  #Nck <-  aggregate(mm$N, by=list(mm$Sample.Label.x), sum)$x
+  #Nc <- sum(Nck)
+
+  ## these are the same, if we calculate Nhatc rather than Nhat
+  ## i.e., ignore the (A/a)^2 multiplier out front
+  #ervar <- #(1/(T*pi*2*max(ptdata.distance$distance)))^2 *
+  #          1/(T*(K-1)) * sum(tk * (Nck/tk - Nc/T)^2)
+
+  #ltervar <- #(1/(2*max(ptdata.distance$distance)*T))^2*
+  #           K/(T^2*(K-1)) * sum(tk^2 * (Nck/tk - Nc/T)^2)
+
+  #varnn <- mrds:::varn(sample$Effort, Nck, type="R2")
+  #varnn3 <- mrds:::varn(tk, Nck, type="R3")
+
+  ## things go bad with uneven effort...
+  #tk <- sample(1:3, K, replace=TRUE)
+  #T <- sum(tk)
+
+  #ervar <- #(1/(T*pi*2*max(ptdata.distance$distance)))^2 *
+  #          1/(T*(K-1)) * sum(tk * (Nck/tk - Nc/T)^2)
+
+  #ltervar <- #(1/(2*max(ptdata.distance$distance)*T))^2*
+  #           K/(T^2*(K-1)) * sum(tk^2 * (Nck/tk - Nc/T)^2)
+
+  #varnn <- mrds:::varn(tk, Nck, type="R2")
+  #varnn3 <- mrds:::varn(tk, Nck, type="R3")
+
+
+})
