5e53f4e147fd36e98f26f2d2466cc9a88884d631
Felix T Petersma <felix.petersma@gmail.com>
2024-05-13 14:46:13 +0200

    Update beta.R

--- R/beta.R
+++ R/beta.R
@@ -20,6 +20,25 @@ beta <- function(int.range, ddfobj, width, standardize = TRUE,
                stdint = stdint, left = left)
   
   ## Derive adj term and series, and key for distance = 0
+  key <- ddfobj$type
+
+  adj.series <- ddfobj$adjustment$series
+  adj.scale <- ddfobj$adjustment$scale
+  adj.order <- ddfobj$adjustment$order
+  adj.parm <- ddfobj$adjustment$parameters
+  adj.exp <- ddfobj$adjustment$exp
+  
+  pars <- getpar(ddfobj)
+  par.indices <- getpar(ddfobj, index = TRUE)
+  if (par.indices[2] != 0) key.scale <- pars[par.indices[2]] else key.scale <- NULL
+  if (par.indices[1] != 0) key.shape <- pars[par.indices[1]] else key.shape <- NULL
+  
+  if(adj.scale == "width"){
+    scaling <- width
+  }else{
+    scaling <- key.scale
+  }
+  
   key.val.0 <- switch(key,
                       hn    = keyfct.hn(0, key.scale),
                       hr    = keyfct.hz(0, key.scale, key.shape),
