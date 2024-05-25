6f54577ad925c06e7aefc83277933b2e44a1f71f
Felix T Petersma <felix.petersma@gmail.com>
2024-05-25 11:15:20 +0200

    Update keyfct.grad.hn.R
    removed the incorrect minus

--- R/keyfct.grad.hn.R
+++ R/keyfct.grad.hn.R
@@ -3,7 +3,7 @@
 #' The key function contains one parameter, the scale. 
 #' Current implementation assumes that scaled dist is x/scale, not x/width
 #' 
-#' d key / d scale = -(exp(-y ^ 2 / (2 scale ^ 2)) * (y ^ 2 / scale ^ 3))
+#' d key / d scale = exp(-y ^ 2 / (2 scale ^ 2)) * (y ^ 2 / scale ^ 3)
 #' 
 #' @param distance perpendicular distance vector
 #' @param key.scale vector of scale values
@@ -11,6 +11,12 @@
 #' @return vector of derivatives of the half-normal key function w.r.t. the
 #' scale parameter
 keyfct.grad.hn <- function(distance, key.scale){
-  return(-(exp(-distance ^ 2 / (2 *  key.scale ^ 2)) *
-             (distance ^ 2 / key.scale ^ 3)))
+  return(exp(-distance ^ 2 / (2 *  key.scale ^ 2)) *
+             (distance ^ 2 / key.scale ^ 3))
+  
+  # # alternative when including the denominator \sqrt{\pi * \sigma ^2 / 2}
+  # DOES NOT WORK
+  # return( 
+  #   -(sqrt(2) * (key.scale^2 - distance ^2) * exp(-distance ^ 2 / (2 *  key.scale ^ 2))
+  #     / sqrt(pi) * key.scale^3 * abs(key.scale)))
 }
