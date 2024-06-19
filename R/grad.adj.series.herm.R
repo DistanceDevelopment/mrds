18c2bc90c44ab5b0bbd36805e4b3d1d1874a4562
Felix T Petersma <felix.petersma@gmail.com>
2024-06-19 10:24:32 +0200

    Update grad.adj.series.herm.R
    only stylistics

--- R/grad.adj.series.herm.R
+++ R/grad.adj.series.herm.R
@@ -5,12 +5,12 @@
 # scaling     - scale parameter
 # adj.order - vector of orders of polynomials to fit
 # adj.parm  - vector of parameters (a_j)
-grad.adj.series.herm <- function(distance, scaling = 1, adj.order, adj.parm=NULL,
-                        adj.exp=FALSE){
+grad.adj.series.herm <- function(distance, scaling = 1, adj.order, 
+                                 adj.parm = NULL, adj.exp = FALSE){
   
   # Check the adjustment parameters
   if(is.null(adj.parm)){
-    adj.parm <- as.vector(rep(1,length(adj.order)))
+    adj.parm <- as.vector(rep(1, length(adj.order)))
   }
   
   adj.order <- as.vector(adj.order)
@@ -18,13 +18,13 @@ grad.adj.series.herm <- function(distance, scaling = 1, adj.order, adj.parm=NULL
   hermsum <- 0
   
   for(i in seq_along(adj.order)){
-    if (adj.order[i-1] == 0) {
+    if (adj.order[i] - 1 == 0) {
       hermsum <- hermsum + 
         (adj.parm[i] * adj.order[i] * 1)
     } else {
       hermsum <- hermsum + 
         (adj.parm[i] * adj.order[i] * hermite.poly((distance / scaling),
-                                                   adj.order[i-1]))
+                                                   adj.order[i] - 1))
     }
   }
   
