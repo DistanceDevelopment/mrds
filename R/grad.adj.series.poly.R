545b9c33e02e647cd75fda8e2a6031ba140165ab
Felix T Petersma <felix.petersma@gmail.com>
2024-06-19 10:24:46 +0200

    Update grad.adj.series.poly.R
    stylistics

--- R/grad.adj.series.poly.R
+++ R/grad.adj.series.poly.R
@@ -5,8 +5,8 @@
 # scaling     - scale parameter
 # adj.order - vector of orders of polynomials to fit
 # adj.parm  - vector of parameters (a_j)
-grad.adj.series.poly <- function(distance, scaling=1, adj.order, adj.parm=NULL,
-                                 adj.exp=FALSE){
+grad.adj.series.poly <- function(distance, scaling = 1, adj.order, 
+                                 adj.parm = NULL, adj.exp = FALSE){
   
   # Check the adjustment parameters
   if(is.null(adj.parm)){
@@ -18,18 +18,18 @@ grad.adj.series.poly <- function(distance, scaling=1, adj.order, adj.parm=NULL,
   polysum <- 0
   
   for(i in seq_along(adj.order)){
-    if (adj.order[i-1] == 0) {
+    if (adj.order[i] - 1 == 0) {
       polysum <- polysum + 
         (adj.parm[i] * adj.order[i] * 1)
     } else {
       polysum <- polysum + 
-        (adj.parm[i] * adj.order[i] * (distance/scaling) ^ adj.order[i-1])
+        (adj.parm[i] * adj.order[i] * (distance/scaling) ^ (adj.order[i] - 1))
     }
   }
   
-  for(i in seq_along(adj.order)){
-    polysum <- polysum + (adj.parm[i]*(distance/scaling)^adj.order[i])
-  }
+  # for(i in seq_along(adj.order)){
+  #   polysum <- polysum + (adj.parm[i]*(distance/scaling) ^ adj.order[i])
+  # }
   
   # if adj.exp return exp(polysum) to keep f(x)>0 
   if(adj.exp){
