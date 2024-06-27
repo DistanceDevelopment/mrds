fafd4717dbf75043b02ed0d3960d1649a957edd5
Felix T Petersma <felix.petersma@gmail.com>
2024-06-27 11:08:55 +0100

    Update integratepdf.grad.R
    removed some unused arguments

--- R/integratepdf.grad.R
+++ R/integratepdf.grad.R
@@ -9,21 +9,25 @@
 #' @param distance vector of distances
 #' @param ddfobj the ddf object
 #' @param width the truncation width
-#' @param left the left truncation (defaults to zero)
+#' @param int.range vector with the lower and upper bound of the integration
+#' @param left the left truncation. Defaults to zero.
+#' @param pdf.based evaluate the non-normalised pdf or the detection function? 
+#'                  Defaults to TRUE. 
+#' @param standardize not being used, can probably be removed.
 #' 
 #' @author Felix Petersma
 
 integratepdf.grad <- function(par.index, ddfobj, int.range, width, 
-                              standardize = FALSE, 
-                              point = FALSE, left = 0, stdint = FALSE, 
-                              select = NULL, index = NULL) {
+                              standardize = FALSE, point = FALSE, left = 0, 
+                              pdf.based = TRUE) {
   
   ## If the non-standardised detection function is required, simply 
   ## integrate the gradient of the detection function directly.
   if (!standardize) {
     out <- integrate(distpdf.grad, lower = int.range[1], upper = int.range[2],
                      par.index = par.index, ddfobj = ddfobj, width = width,
-                     standardize = FALSE, point = point, left = left)$value
+                     standardize = FALSE, point = point, left = left,
+                     pdf.based = pdf.based)$value
   } 
   
   return(out)
