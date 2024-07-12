29c84ce47c9772ac0e57c03d91711c62c32cc767
Felix T Petersma <felix.petersma@gmail.com>
2024-07-12 14:04:12 +0200

    moved to mrds/R

---
+++ R/plotDetfct.R
@@ -0,0 +1,138 @@
+#' This function is used plot a bunch of detection functions that are key+adj.
+#' Covariates are not permitted. The detection function consists of 
+#' a key function and adjustment series. The permitted key functions are: 
+#' uniform ('unif'), half-normal ('hn') and hazard-rate ('hr'). The permitted 
+#' adjustment series are cosine ('cos'), simple polynomial ('poly') and 
+#' Hermite polynomial ('herm'). The maximum number of adjustment terms is 5, 
+#' which means that the maximum number of parameters in a detection function is 
+#' 7, which is when the key is hazard-rate. 
+#' 
+#' The permitted orders of adjustment terms depend on the key-adjustment 
+#' combination:
+#' Key          Adjustment          orders
+#' unif         cos                 1, 2, 3, 4, 5
+#' unif         poly                2, 4, 6, 8, 10
+#' unif         herm                2, 4, 6, 8, 10
+#' hn           cos                 2, 3, 4, 5, 6
+#' hn           poly                4, 6, 8, 10, 12
+#' hn           herm                4, 6, 8, 10, 12
+#' hr           cos                 2, 3, 4, 5, 6
+#' hr           poly                4, 6, 8, 10, 12
+#' hr           herm                4, 6, 8, 10, 12
+#' 
+#' @param specs a data frame with the columns:
+#'   'key' is the key function, 'adj' is the adjustment series, 'scale' is the 
+#'   scale paramter (can be NA), 'shape' is the shape parameter (can be NA),
+#'   'adj1--adj5' are the adjustment coefficients (can be NA but not all) and 
+#'   the orders will be determined based on which coefficients are provided and
+#'   the key+adjustment combination, 'left' is the left truncation and 'width'
+#'    is the right truncation distance, and scaling. 
+#' @param pts the number of distances between min
+#' 
+plotDetfct <- function(specs, pts = 10000) {
+  
+  # specs <- data.frame(
+  #   key = "hn",
+  #   adj = "cos",
+  #   scale = 1.5,
+  #   shape = NA,
+  #   adj1 = 0.5,
+  #   adj2 = 1,
+  #   adj3 = 0.4,
+  #   adj4 = NA,
+  #   adj5 = NA,
+  #   left = 0,
+  #   width = 100,
+  #   scaling = 'width'
+  # )
+
+  
+  for (i in 1:nrow(specs)) {
+    
+    readline(prompt = paste0("Press [enter] to print plot ", i, ":"))
+    
+    key <- specs[i, 1]
+    adj <- specs[i, 2]
+    left <- specs[i, "left"]
+    width <- specs[i, "width"]
+    scaling <- specs[i, "scaling"]
+    ## Check validity of detection function 
+    if (key == "unif" & adj == "cos") {
+      orders <- c(1, 2, 3, 4, 5)[!is.na(specs[i, c(5:9)])]
+    }
+    if (key != "unif" & adj == "cos") {
+      orders <- c(2, 3, 4, 5, 6)[!is.na(specs[i, c(5:9)])]
+    }
+    if (key == "unif" & adj %in% c("poly", "herm")) {
+      orders <- c(2, 4, 6, 8, 10)[!is.na(specs[i, c(5:9)])]
+    } 
+    if (key != "unif" & adj %in% c("poly", "herm")) {
+      orders <- c(4, 6, 8, 10, 12)[!is.na(specs[i, c(5:9)])]
+    } 
+    
+    parameters <- specs[i, c(3:9)][!is.na(specs[i, c(3:9)])]
+
+    ## Extract the parameters
+    if (key == "unif") {
+      pars <- list(adjustment = parameters)
+    } else if (key == "hn") {
+      pars <- list(scale = parameters[1], 
+                   adjustment = parameters[-1])
+    } else {
+      pars <- list(shape = parameters[1],
+                   scale = parameters[2], 
+                   adjustment = parameters[-c(1,2)])
+    }
+    
+    ## Create a fake ddfobj to pass onto distpdf()
+    distance <- seq(from = left, to = width, length = pts)
+    # ddfobj <- ddf(dsmodel = ~mcds(key = key,
+    #                               formula = ~1,
+    #                               adj.series = adj,
+    #                               adj.order = orders,
+    #                               adj.scale = scaling),
+    #               data = data.frame(distance = distance,
+    #                                 object = 1:pts,
+    #                                 observer = 1,
+    #                                 detected = 1),
+    #               method = "ds",
+    #               meta.data = list(
+    #                                left = left,
+    #                                width = width, 
+    #                                mono = TRUE),
+    #               control = list(nofit = TRUE, 
+    #                              optimizer = "R",
+    #                              initial = pars))
+    ddfobj <- list()
+    ddfobj$type <- key
+    if (key %in% c("hn", "hr")) {
+      ddfobj$scale$dm <- rep(1, pts)
+      ddfobj$scale$parameters <- pars$scale
+    }
+    if (key == "hr") {
+      ddfobj$shape$dm <- rep(1, pts)
+      ddfobj$shape$parameters <- pars$shape
+    }
+    
+    ddfobj$adjustment$series <- adj
+    ddfobj$adjustment$scale <- scaling
+    ddfobj$adjustment$order <- orders
+    ddfobj$adjustment$parameters <- pars$adjustment
+    ddfobj$adjustment$exp <- FALSE
+    
+    ## Derive non-normalised pdfs for the distances
+    g <- detfct(distance = distance, ddfobj = ddfobj,
+                        left = left, width = width, standardize = TRUE)
+    
+    col <- rep("black", pts)
+    col[g < 0] <- "red"
+    plot(y = g, x = distance, col = col)
+    
+    if (any(g < 0)) {
+      cat("The pdf is negative for some distances between left and width.\n")
+      cat("This is indicated in red.\n")
+    }
+    
+    
+  }
+}
---
+++ R/simDists.R
@@ -0,0 +1,137 @@
+#' This function is used to simulated distances from a detection with 
+#' adjustments. Covariates are not permitted. The detection function consists of 
+#' a key function and adjustment series. The permitted key functions are: 
+#' uniform ('unif'), half-normal ('hn') and hazard-rate ('hr'). The permitted 
+#' adjustment series are cosine ('cos'), simple polynomial ('poly') and 
+#' Hermite polynomial ('herm'). The maximum number of adjustment terms is 5, 
+#' which means that the maximum number of parameters in a detection function is 
+#' 7, which is when the key is hazard-rate. 
+#' 
+#' The permitted orders of adjustment terms depend on the key-adjustment 
+#' combination:
+#' Key          Adjustment          orders
+#' unif         cos                 1, 2, 3, 4, 5
+#' unif         poly                2, 4, 6, 8, 10
+#' unif         herm                2, 4, 6, 8, 10
+#' hn           cos                 2, 3, 4, 5, 6
+#' hn           poly                4, 6, 8, 10, 12
+#' hn           herm                4, 6, 8, 10, 12
+#' hr           cos                 2, 3, 4, 5, 6
+#' hr           poly                4, 6, 8, 10, 12
+#' hr           herm                4, 6, 8, 10, 12
+#' 
+#' @param n the number of distances to be simulated
+#' @param key character indicating the key function
+#' @param adj character indicating the adjustment series
+#' @param orders vector of orders. Minimum length is 1, max length is 5
+#' @param width the width or right truncation distance
+#' @param left the left truncation distance (default is 0)
+#' @param point boolean for point or line transect data (default is TRUE)
+#' @param pts the number of distances between min
+#' 
+#' 
+#' 
+#' 
+simDists <- function(n, key, adj, orders, parameters, 
+                     width, scaling = "width", left = 0, 
+                     mono = TRUE, mono_strict = TRUE, point = TRUE, 
+                     pts = 10000) {
+  n <- 100; key <- "hn"; adj <- "cos"; orders <- c(2, 3, 4); width <- 100
+  scaling <- "width"; left <- 0; mono <- TRUE; mono_strict <- TRUE
+  point <- TRUE; pts <- 10000; parameters <- c(1.5, 0.5, 1, 0.4)
+  
+  ## Unit testing
+  if (n <= 0 | is.infinite(n)) {
+    stop("n has to be positive and real.")
+  }
+  if (!key %in% c("unif", "hn", "hr")){
+    stop("key has to be either 'unif', 'hn' or 'hr'.")
+  }
+  if (!adj %in% c("cos", "poly", "herm")){
+    stop("key has to be either 'cos', 'poly' or 'herm'.")
+  }
+  if (length(orders) < 1 | length(orders) > 5) {
+    stop("A minimum of 1 and maximum of 5 adjustment terms are permitted.")
+  }
+
+  ## Check validity of detection function 
+  if (key == "unif" & adj == "cos" & !all(orders %in% c(1, 2, 3, 4, 5))) {
+    stop("The unif+cos combination only allows orders 1, 2, 3, 4 and 5.")
+  }
+  if (key == "hn" & adj == "cos" & !all(orders %in% c(2, 3, 4, 5, 6))) {
+    stop("The hn+cos combination only allows orders 2, 3, 4, 5 and 6.")
+  }
+  if (key == "hr" & adj == "cos" & !all(orders %in% c(2, 3, 4, 5, 6))) {
+    stop("The hr+cos combination only allows orders 2, 3, 4, 5 and 6.")
+  }
+  
+  if (key == "unif" & adj %in% c("poly", "herm") & 
+      !all(orders %in% c(2, 4, 6, 8, 10))) {
+    stop("The unif+poly and unif+herm combinations only allow orders 2, 4, 6, 8 and 10.")
+  }
+  
+  if (key != "unif" & adj %in% c("poly", "herm") & 
+      !all(orders %in% c(4, 6, 8, 10, 12))) {
+    stop("The hn+poly, hn+herm, hr+poly and hr+herm combinations only allow orders 4, 6, 8, 10 and 12.")
+  }
+  
+  ## Extract the parameters
+  if (key == "unif") {
+    pars <- list(adjustment = parameters)
+  } else if (key == "hn") {
+    pars <- list(scale = parameters[1], 
+                adjustment = parameters[-1])
+  } else {
+    pars <- list(shape = parameters[1],
+                 scale = parameters[2], 
+                 adjustment = parameters[-c(1,2)])
+  }
+  
+  ## Create a fake ddfobj to pass onto distpdf()
+  distance <- seq(from = left, to = width, length = pts)
+  # ddfobj <- ddf(dsmodel = ~mcds(key = key,
+  #                               formula = ~1,
+  #                               adj.series = adj,
+  #                               adj.order = orders,
+  #                               adj.scale = scaling),
+  #               data = data.frame(distance = distance,
+  #                                 object = 1:pts,
+  #                                 observer = 1,
+  #                                 detected = 1),
+  #               method = "ds",
+  #               meta.data = list(point = point, 
+  #                                left = left,
+  #                                width = width, 
+  #                                mono = mono, 
+  #                                mono.strict = mono_strict),
+  #               control = list(nofit = TRUE, 
+  #                              optimizer = "R",
+  #                              initial = pars))
+  ddfobj <- list()
+  ddfobj$type <- key
+  if (key %in% c("hn", "hr")) {
+    ddfobj$scale$dm <- rep(1, pts)
+    ddfobj$scale$parameters <- pars$scale
+    }
+  if (key == "hr") {
+    ddfobj$shape$dm <- rep(1, pts)
+    ddfobj$shape$parameters <- pars$shape
+  }
+  
+  ddfobj$adjustment$series <- adj
+  ddfobj$adjustment$scale <- scaling
+  ddfobj$adjustment$order <- orders
+  ddfobj$adjustment$parameters <- pars$adjustment
+  ddfobj$adjustment$exp <- FALSE
+  
+  ## Derive non-normalised pdfs for the distances
+  dist_pdf <- distpdf(distance = distance, ddfobj = ddfobj, point = point, 
+                      left = left, width = width)
+  
+  if (any(dist_pdf < 0)) {
+    cat("The pdf is negative for some distances between left and width! These were capped at 0.\n")
+    dist_pdf[dist_pdf < 0] <- 0
+  }
+  
+  sampled_dists <- sample(distance, size = n, replace = TRUE, prob = dist_pdf)
+}
